a51d60b5845f7123aa873635feaaf29254442ee8
gohanica <goatasparagus0112@gmail.com>
2020-02-11 12:36:50 +0900

    Signed-off-by: gohanica <goatasparagus0112@gmail.com>

--- crawler.go
+++ crawler.go
@@ -7,11 +7,17 @@ import (
 func Crawler() {
 	t := time.NewTicker(5 * time.Second)
 	defer t.Stop()
-	layout := time.
+	layout := time.RFC1123Z
 	for {
 		select {
 		case <-t.C:
 		now:=time.Now()
+		date:=date_from_db()
+		var format_date:=[]time.Time
+		for i range date{
+		format_date
+		}
+		if
 		
 
 		}
