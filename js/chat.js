becb35eadfac4d4d8a28f6314872afb5fbf62e11
watagori <54665270+watagori@users.noreply.github.com>
2020-02-12 01:22:06 +0900

    HTMLCSSJavaScript使用

--- js/chat.js
+++ js/chat.js
@@ -1,35 +1,20 @@
-var now = new Date();
-var month = now.getMonth() + 1;
-var nowtime = now.getFullYear() + "/" + month + "/" + now.getDate() + "/" + now.getHours() + ":" + now.getMinutes();
-var comment = {
-    profileimage: "../image/1.png",
+let now = new Date();
+let month = now.getMonth() + 1;
+let nowtime = now.getFullYear() + "/" + month + "/" + now.getDate() + "/" + now.getHours() + ":" + now.getMinutes();
+let comment = {
+    profileimage: "../image/0.png",
     name: "大谷翔平",
     time: nowtime,
     id: 17123456789,
     text: "google最強！"
 }
 
-// 要素へデータ入れる
-// var userimage = document.getElementById('image');
-// var username = document.getElementById('name');
-// var usertime = document.getElementById('time');
-// var userid = document.getElementById('id');
-// var usertext = document.getElementById('text');
-// var userinfo = document.getElementById('userinfo');
-// userimage.src = comment.profileimage
-// username.innerHTML = comment.name;
-// usertime.textContent = comment.time;
-// userid.textContent = comment.id;
-// usertext.textContent = comment.text;
-
-
-
 // 送信系HTML取得
-var input = document.getElementById('input');
-var username = document.getElementById('username');
+let input = document.getElementById('input');
+let username = document.getElementById('username');
 
 // websocket通信開始
-var socket = new WebSocket("ws://" + window.location.host + "/chat");
+let socket = new WebSocket("ws://" + window.location.host + "/chat");
 
 // websocketクライアントからサーバーへ送信
 function send() {
@@ -44,49 +29,100 @@ function send() {
 };
 
 
-var userdata = [];
+let userdata = [];
 // websocket サーバからメッセージ受信
 socket.onmessage = function (e) {
-    var js = JSON.parse(e.data);
+
+
+
+    // 実験
+    let comments = document.getElementsByClassName("comment");
+
+
+
+    let js = JSON.parse(e.data);
 
     // テンプレートデータ取得整形
     userdata = { Message: js.message, Username: js.username }
 
     //    スレッドDOM追加
     // bodyにdivタグ追加
-    var newdiv = document.createElement('div');
-    var textdiv = document.createElement('div');
+    let newdiv = document.createElement('div');
+    let textdiv = document.createElement('div');
+    let buttondiv = document.createElement('div');
     document.body.appendChild(newdiv);
     document.body.appendChild(textdiv);
-    // spanタグ用意
-    // var newimagesss = ducument.createElement('img');
-    var newimage = document.createElement('img');
-    var newname = document.createElement('span');
-    var newtime = document.createElement('span');
-    var newid = document.createElement('span');
+    document.body.appendChild(buttondiv);
 
-    // divタグの中にspanタグ追加
+    // span・img・inputタグ用意
+    let newimage = document.createElement('img');
+    let newname = document.createElement('span');
+    let newtime = document.createElement('span');
+    let newid = document.createElement('span');
+    let newgood = document.createElement('input');
+    let newbad = document.createElement('input');
+
+    // divタグの中にspanタグinputタグ追加
     newdiv.appendChild(newimage);
     newdiv.appendChild(newname);
     newdiv.appendChild(newtime);
     newdiv.appendChild(newid);
+    buttondiv.appendChild(newgood);
+    buttondiv.appendChild(newbad);
+
+    // ユーザー情報
+    newname.setAttribute("class", "newname");
+    newtime.setAttribute("class", "newtime");
+    newid.setAttribute("class", "newid");
 
 
     // 追加するもの準備
+    //プロフィール画像 
     newimage.src = comment.profileimage;
     newimage.setAttribute("width", "40");
     newimage.setAttribute("height", "40");
     newimage.setAttribute("align", "middle");
-    var divname = document.createTextNode(userdata.Username);
-    var divtime = document.createTextNode(comment.time + " ID:");
-    var divid = document.createTextNode(comment.id);
-    textdiv.innerHTML = userdata.Message;
+    // ユーザー情報・コメント
+    let children = document.body.childNodes;
+    let divname = document.createTextNode(userdata.Username);
+    let divtime = document.createTextNode(comment.time);
+    let divid = document.createTextNode(comment.id);
+    textdiv.innerHTML = userdata.Message + ":commentclass:" + comments.length + " Bodychild:" + children.length;
+    textdiv.setAttribute("class", "comment");
+    // いいねだめだね機能
+    newgood.setAttribute("type", "button");
+    newbad.setAttribute("type", "button");
+    newgood.setAttribute("onclick", "goodcount()");
+    newbad.setAttribute("onclick", "badcount()");
+    newgood.setAttribute("id", "good");
+    newbad.setAttribute("id", "bad");
 
     // spanタグの中に情報追加
     newname.appendChild(divname);
     newtime.appendChild(divtime);
     newid.appendChild(divid);
+
+
+
+
+    // ↑onmesssageここまで
 };
+
+
+
+// イイね機能
+let goodcounts = 0;
+let badcounts = 0;
+function goodcount() {
+    goodcounts++;
+    document.getElementById("good").value = goodcounts;
+
+}
+function badcount() {
+    badcounts++;
+    document.getElementById("bad").value = badcounts;
+}
+
 
 
 
--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("js/"))))
 	http.Handle("/templates/", http.StripPrefix("/templates/", http.FileServer(http.Dir("templates/"))))
 	http.Handle("/image/", http.StripPrefix("/image/", http.FileServer(http.Dir("image/"))))
-
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css/"))))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "templates/chat.html")
 	})
