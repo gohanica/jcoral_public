b32ec6433b65aa279c6e00be2ab220466f11c075
watagori <54665270+watagori@users.noreply.github.com>
2020-02-17 14:50:56 +0900

    second!

--- main.go
+++ main.go
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-//イキスギィ
-=======
 package main
 
 import "fmt"
@@ -8,4 +5,3 @@ import "fmt"
 func main() {
 	fmt.Println("Hello,Worldder")
 }
->>>>>>> c09e59c94bb8689fe3de8ca13af158a689a42cc6
