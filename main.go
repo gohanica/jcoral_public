3d18cd0383f4c7da42ad77f701596c48d947ace8
watagori <54665270+watagori@users.noreply.github.com>
2020-02-23 23:32:11 +0900

    本：静的ファイルの基本的な構造 ・6個以下の構造体を含んだ構造体の配列を作成→Execute ・構造体に含まれるフィールド名はMessage,Date,ID,Profile,Username ・構造体のスライスの配列一つ一つが６個になる必要はない→空を入れる必要なし ・日付の付け方が未実装後日変更あり

--- main.go
+++ main.go
@@ -16,6 +16,13 @@ type Comment struct {
 	Date     string `json:"date"`
 	Profile  string `json:"profile"`
 	Username string `json:"username"`
+	// Heading  string
+}
+
+// Onepage 一ページに表示される内容
+type Onepage struct {
+	Comment []Comment
+	Heading string
 }
 
 func main() {
@@ -41,11 +48,32 @@ func templ(w http.ResponseWriter, r *http.Request) {
 	var tcomment Comment
 	tcomment.Message = "おばけほーく"
 	tcomment.Date = "2020/02/23/23:13"
-	tcomment.ID = "19930130"
-	tcomment.Profile = "../image/1.png"
+	tcomment.ID = "ID:19930130"
+	tcomment.Profile = "../image/0.png"
 	tcomment.Username = "千賀滉大"
-	tcomments := []Comment{tcomment, tcomment}
-	t.Execute(w, tcomments)
+	// tcomment.Heading = "2020/02/23"
+
+	var tcomment2 Comment
+	tcomment2.Message = "おばけほーく２"
+	tcomment2.Date = "2020/02/23/23:13"
+	tcomment2.ID = "ID:19930130"
+	tcomment2.Profile = "../image/1.png"
+	tcomment2.Username = "千賀滉大２"
+	// tcomment2.Heading = "2020/02/24"
+
+	tcomments := []Comment{tcomment, tcomment2, tcomment2}
+	tcomments2 := []Comment{tcomment, tcomment2, tcomment, tcomment2}
+
+	var onepage1 Onepage
+	onepage1.Comment = tcomments
+	onepage1.Heading = "2020/02/23"
+
+	var onepage2 Onepage
+	onepage2.Comment = tcomments2
+	onepage2.Heading = "2020/02/24"
+	// tcommentss := []Onepage{onepage1, onepage2}
+	tcommentss := [][]Comment{tcomments, tcomments2}
+	t.Execute(w, tcommentss)
 }
 
 // 解析用ハンドラ
